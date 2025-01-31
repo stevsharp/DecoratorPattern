12c5599bd3e74824848e69493a550327de7e9521
SPYROS\Spyros <sponaris@gmail.com>
2025-01-31 22:40:07 +0200

    Decorator Patten Using Autofac

---
+++ DecoratorPatternUsingAutoFac/ConsoleLoggerService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DecoratorPatternUsingAutoFac
+{
+    public class ConsoleLoggerService : ILoggerService
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[ConsoleLogger] {message}");
+        }
+    }
+}
